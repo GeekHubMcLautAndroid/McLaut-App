dc4c8f4aa6444cb2f6fda3c0f1edd0522d046638
Bohdan <bogdanrybak1996@gmail.com>
2017-11-23 23:33:13 +0200

    write temp repository methods

--- app/src/main/java/ua/ck/android/geekhub/mclaut/data/Repository.java
+++ app/src/main/java/ua/ck/android/geekhub/mclaut/data/Repository.java
@@ -3,6 +3,9 @@ package ua.ck.android.geekhub.mclaut.data;
 import android.arch.lifecycle.MutableLiveData;
 
 import ua.ck.android.geekhub.mclaut.data.entities.LoginResultInfo;
+import ua.ck.android.geekhub.mclaut.data.entities.PaymentsListEntity;
+import ua.ck.android.geekhub.mclaut.data.entities.UserInfoEntity;
+import ua.ck.android.geekhub.mclaut.data.entities.WithdrawalsListEntity;
 import ua.ck.android.geekhub.mclaut.data.network.NetworkDataSource;
 
 public class Repository {
@@ -20,8 +23,29 @@ public class Repository {
     }
 
     public MutableLiveData<LoginResultInfo> getLoginInfo(String login, String password, int city){
-        MutableLiveData<LoginResultInfo> loginResultsLiveData = NetworkDataSource.getInstance().
-                checkLogin(login,password,city);
-        return loginResultsLiveData;
+
+        return NetworkDataSource.getInstance().checkLogin(login,password,city);
     }
+
+    public MutableLiveData<UserInfoEntity> getUserInfo(String certificate, int city){
+
+        //TODO: get user info from local database
+        //Temp return from network
+        return NetworkDataSource.getInstance().getUserInfo(certificate,city);
+    }
+
+    public MutableLiveData<WithdrawalsListEntity> getWithdrawalsInfo(String certificate, int city){
+
+        //TODO: get withdrawals from local database
+        //Temp return from network
+        return NetworkDataSource.getInstance().getWithdrawals(certificate,city);
+    }
+    public MutableLiveData<PaymentsListEntity> getPaymentsInfo(String certificate, int city){
+
+        //TODO: get withdrawals from local database
+        //Temp return from network
+        return NetworkDataSource.getInstance().getPayments(certificate,city);
+    }
+
+    //TODO: Write new information from network to local database
 }
