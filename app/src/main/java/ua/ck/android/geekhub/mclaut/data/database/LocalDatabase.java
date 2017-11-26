60298a6652a36ab5a9ced52859c7142e140c33bd
SergeySmikovskiy <sergeuwork23@gmail.com>
2017-11-26 19:15:49 +0200

    Add MIGRATION for database

--- app/src/main/java/ua/ck/android/geekhub/mclaut/data/database/LocalDatabase.java
+++ app/src/main/java/ua/ck/android/geekhub/mclaut/data/database/LocalDatabase.java
@@ -1,12 +1,15 @@
 package ua.ck.android.geekhub.mclaut.data.database;
 
+import android.arch.persistence.db.SupportSQLiteDatabase;
 import android.arch.persistence.db.SupportSQLiteOpenHelper;
 import android.arch.persistence.room.Database;
 import android.arch.persistence.room.DatabaseConfiguration;
 import android.arch.persistence.room.InvalidationTracker;
 import android.arch.persistence.room.Room;
 import android.arch.persistence.room.RoomDatabase;
+import android.arch.persistence.room.migration.Migration;
 import android.content.Context;
+import android.support.annotation.NonNull;
 
 import ua.ck.android.geekhub.mclaut.data.entities.CardInfoEntity;
 import ua.ck.android.geekhub.mclaut.data.entities.CashTransactionsEntity;
@@ -20,7 +23,7 @@ import ua.ck.android.geekhub.mclaut.data.entities.UserInfoEntity;
 @Database(entities = {UserInfoEntity.class,
                       UserConnectionsInfo.class,
                       CashTransactionsEntity.class,
-                      CardInfoEntity.class}, version = 1)
+                      CardInfoEntity.class}, version = 2)
 
 public abstract class LocalDatabase extends RoomDatabase {
 
@@ -28,18 +31,30 @@ public abstract class LocalDatabase extends RoomDatabase {
     private static final Object LOCK = new Object();
     private static volatile LocalDatabase instance;
 
+    private static final int lastVersion = 1;
+    private static final int newVersion = 2;
+
+    private static Migration MIGRATION = new Migration(lastVersion,newVersion) {
+        @Override
+        public void migrate(@NonNull SupportSQLiteDatabase database) {}
+    };
+
     public  static  LocalDatabase getInstance(Context context){
         if(instance == null){
             synchronized (LOCK){
                 if(instance == null){
                     instance = Room.databaseBuilder(context.getApplicationContext(),
                             LocalDatabase.class,
-                            LocalDatabase.DATABASE_NAME).build();
+                            LocalDatabase.DATABASE_NAME)
+                            .addMigrations(MIGRATION)
+                            .build();
                 }
             }
         }
         return instance;
     }
+
+
 
     public abstract Dao dao();
 
