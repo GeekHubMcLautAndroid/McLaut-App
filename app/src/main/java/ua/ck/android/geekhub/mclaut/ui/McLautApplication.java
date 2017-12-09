2b55940e42455efa18085fdeacc5387a82208281
SergeySmikovskiy <sergeuwork23@gmail.com>
2017-12-10 00:05:11 +0200

    Add new class

---
+++ app/src/main/java/ua/ck/android/geekhub/mclaut/ui/McLautApplication.java
@@ -0,0 +1,53 @@
+package ua.ck.android.geekhub.mclaut.ui;
+
+import android.app.Application;
+import android.content.Context;
+import android.content.SharedPreferences;
+
+/**
+ * Created by Sergo on 12/9/17.
+ */
+
+public class McLautApplication extends Application{
+
+    private static final String USER_ID = "_user_id";
+    private static final String MCLAUT_PREFERENCES = "_mclaut_preferences";
+
+    private static McLautApplication instance;
+    private static SharedPreferences preferences;
+    private static SharedPreferences.Editor editor;
+
+    public static McLautApplication getInstance() {
+        return instance;
+    }
+
+    public static Context getContext() {
+        return instance.getBaseContext();
+    }
+
+    public static void selectUser(String userId){
+        editor.putString(USER_ID, userId).commit();
+    }
+
+    public static void getSelectedUser(String userId){
+        preferences.getString(USER_ID, "NULL");
+    }
+
+    @Override
+    public void onCreate(){
+        instance = this;
+        initPreferences();
+        initPreferencesEditor();
+
+        super.onCreate();
+    }
+
+    private static void initPreferences(){
+        preferences = instance.getSharedPreferences(MCLAUT_PREFERENCES, MODE_PRIVATE);
+    }
+
+    private static void initPreferencesEditor(){
+        editor = preferences.edit();
+    }
+
+}
