64908e6cf0cddb34ce7576da09c8aaea5146b8e7
Serhiy Kovernik <Serhiy-120397@ukr.net>
2017-12-23 18:52:48 +0200

    Remove old fragment files. Create xml directory and blank file for settings.

--- app/src/main/java/ua/ck/android/geekhub/mclaut/ui/settings/SettingsFragment.java
+++ app/src/main/java/ua/ck/android/geekhub/mclaut/ui/settings/SettingsFragment.java
@@ -1,29 +1,15 @@
 package ua.ck.android.geekhub.mclaut.ui.settings;
 
-import android.arch.lifecycle.ViewModelProviders;
 import android.os.Bundle;
-import android.support.annotation.Nullable;
-import android.support.v4.app.Fragment;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
+import android.preference.PreferenceFragment;
 
 import ua.ck.android.geekhub.mclaut.R;
-import ua.ck.android.geekhub.mclaut.ui.settings.SettingsViewModel;
 
-/**
- * Created by Sergo on 11/24/17.
- */
+public class SettingsFragment extends PreferenceFragment {
 
-public class SettingsFragment extends Fragment {
-
-    private SettingsViewModel viewModel;
-
-    @Nullable
     @Override
-    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        viewModel = ViewModelProviders.of(this).get(SettingsViewModel.class);
-
-        return inflater.inflate(R.layout.fragment_settings,container,false);
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        addPreferencesFromResource(R.xml.settings);
     }
 }
