4eb7579db6a6e6cec2b9dba6e7fed80fadba966a
Serhiy Kovernik <Serhiy-120397@ukr.net>
2018-01-11 18:46:56 +0200

    Fix web_view save.

--- app/src/main/java/ua/ck/android/geekhub/mclaut/ui/tachcardPay/TachcardPayStep2Fragment.java
+++ app/src/main/java/ua/ck/android/geekhub/mclaut/ui/tachcardPay/TachcardPayStep2Fragment.java
@@ -1,8 +1,9 @@
 package ua.ck.android.geekhub.mclaut.ui.tachcardPay;
 
-import android.support.v4.app.Fragment;
+import android.annotation.SuppressLint;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -10,20 +11,38 @@ import android.webkit.WebView;
 
 public class TachcardPayStep2Fragment extends Fragment {
 
+    WebView outWebView;
     String location;
     String html;
 
+    @SuppressLint("SetJavaScriptEnabled")
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
         Bundle argument = getArguments();
         location = argument.getString("location");
         html = argument.getString("html");
-        WebView outWebView = new WebView(inflater.getContext());
+        outWebView = new WebView(inflater.getContext());
         outWebView.getSettings().setJavaScriptEnabled(true);
-        String mine = "text/html";
-        String encoding = "UTF-8";
-        outWebView.loadDataWithBaseURL(location, html, mine, encoding, null);
         return outWebView;
+    }
+
+
+    @Override
+    public void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        outWebView.saveState(outState);
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        if (savedInstanceState == null) {
+            String mine = "text/html";
+            String encoding = "UTF-8";
+            outWebView.loadDataWithBaseURL(location, html, mine, encoding, null);
+        } else {
+            outWebView.restoreState(savedInstanceState);
+        }
     }
 }
